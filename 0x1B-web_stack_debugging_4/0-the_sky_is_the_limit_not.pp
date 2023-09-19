60c21c2b604f47aa042809e23753f5d86c5e2b17
Alfatiha <alfatihaahmed@gmail.com>
2023-09-19 04:49:05 +0000

    added fixes

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# fix failed requests
+exec {'file limit for nginx':
+  command => 'sed -i "s/15/2000/g" /etc/default/nginx',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+  onlyif  => 'test -f /etc/default/nginx'
+}
